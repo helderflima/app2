45c439211ea1f45880b4035fdd3305c8084e61c2
Helder Lima <helderfrali@gmail.com>
2015-06-02 17:27:03 -0300

    curso-django-33%

--- artigos/models.py
+++ artigos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models import permalink
 
 # Create your models here.
+
+
+class Agencia(models.Model):
+    nome = models.CharField(max_length=50)
+    site = models.URLField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Artigo(models.Model):
+    titulo = models.CharField('Título:', max_length=80)
+    url = models.SlugField(
+            'URL',
+            max_length=
+    )
