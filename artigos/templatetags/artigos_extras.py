b0375443f1f7f57f6b50b9d609a4ff509e5020ea
Helder Lima <helderfrali@gmail.com>
2015-06-14 15:00:53 -0300

    curso-django-59% - [comments]

--- artigos/templatetags/artigos_extras.py
+++ artigos/templatetags/artigos_extras.py
@@ -1,5 +1,7 @@
 __author__ = 'Helder'
 from django import template
+import re
+from django.utils.safestring import mark_safe
 import datetime
 
 register = template.Library()
@@ -26,3 +28,37 @@ def do_current_time(parser, token):
         msg = '%r tag requer um argumento simples' % token.split_contents()[0]
         raise template.TemplateSyntaxError(msg)
     return CurrentTimeNode(format_string[1:-1])
+
+
+class_re = re.compile(r'(?<=class=["\'])(.*)(?=["\'])')
+
+def add_class(value, css_class):
+    string = str(value)
+    match = class_re.search(string)
+    if match:
+        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (css_class, css_class,
+                                                    css_class, css_class), match.group(1))
+        if not m:
+            return mark_safe(class_re.sub(match.group(1) + " " + css_class,
+                                          string))
+    else:
+        return mark_safe(string.replace('>', ' class="%s">' % css_class))
+    return value
+
+placeholder_re = re.compile(r'(?<=placeholder=["\'])(.*)(?=["\'])')
+
+def add_placeholder(value, placeholder_text):
+    string = str(value)
+    match = placeholder_re.search(string)
+    if match:
+        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (placeholder_text, placeholder_text,
+                                                    placeholder_text, placeholder_text), match.group(1))
+        if not m:
+            return mark_safe(placeholder_re.sub(match.group(1) + " " + placeholder_text,
+                                          string))
+    else:
+        return mark_safe(string.replace('>', ' placeholder="%s">' % placeholder_text))
+    return value
+
+register.filter('add_class', add_class)
+register.filter('add_placeholder', add_placeholder)
